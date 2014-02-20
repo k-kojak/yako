05b58fd10ed37c8b428c4e700845b01b6c317257
Wilike <vilmos.wieszner@gmail.com>
2014-02-20 12:59:57 +0100

    change auto escape place

--- src/hu/rgai/android/eventlogger/rsa/RSAENCODING.java
+++ src/hu/rgai/android/eventlogger/rsa/RSAENCODING.java
@@ -19,18 +19,18 @@ public enum RSAENCODING {
 
   static private final BigInteger modulus = new BigInteger(
       "110019859073945242510877405778084481570732522148482645736391659659567807724977438079018362919244660194551718298987676235910517060152853035227390309704170871426389401061247064442968568023533222336262094702136640899204593996808788636121771395089960420082741676536557880104685444709096373538148003019718221305213");
-  static private final BigInteger exponent = new BigInteger("65537");
+  static private final BigInteger exponent = new BigInteger( "65537");
 
-  static RSAPublicKeySpec publicKeySpec = new RSAPublicKeySpec(modulus, exponent);
+  static RSAPublicKeySpec publicKeySpec = new RSAPublicKeySpec( modulus, exponent);
   static RSAPublicKey publicKey = null;
   static Cipher cypher = null;
   static {
 
     try {
-      KeyFactory fact = KeyFactory.getInstance("RSA");
-      publicKey = (RSAPublicKey) fact.generatePublic(publicKeySpec);
-      cypher = Cipher.getInstance("RSA/ECB/NoPadding");
-      cypher.init(Cipher.ENCRYPT_MODE, publicKey);
+      KeyFactory fact = KeyFactory.getInstance( "RSA");
+      publicKey = (RSAPublicKey) fact.generatePublic( publicKeySpec);
+      cypher = Cipher.getInstance( "RSA/ECB/NoPadding");
+      cypher.init( Cipher.ENCRYPT_MODE, publicKey);
     } catch (InvalidKeySpecException e) {
       // TODO Auto-generated catch block
       e.printStackTrace();
@@ -50,10 +50,10 @@ public enum RSAENCODING {
   private RSAENCODING() {
   }
 
-  public String encodingString(String log) {
+  public String encodingString( String log) {
 
     try {
-      return new String(cypher.doFinal(log.getBytes("utf-8")));
+      return new String( cypher.doFinal( log.getBytes( "utf-8")));
     } catch (IllegalBlockSizeException e) {
       // TODO Auto-generated catch block
       e.printStackTrace();
