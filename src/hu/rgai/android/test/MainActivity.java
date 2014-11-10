66691feee3697da7be14458845eeb0df26b22978
Tamas Kojedzinszky <tamas.kojedzinszky@gmail.com>
2014-11-10 12:52:30 +0100

    Navigation drawer icon changed (lightened).

--- src/hu/rgai/android/test/MainActivity.java
+++ src/hu/rgai/android/test/MainActivity.java
@@ -18,7 +18,6 @@ import android.support.v4.app.FragmentTransaction;
 import android.support.v4.content.LocalBroadcastManager;
 import android.support.v4.view.GravityCompat;
 import android.support.v4.widget.DrawerLayout;
-import android.support.v7.app.ActionBarActivity;
 import android.util.Log;
 import android.view.*;
 import android.view.animation.Animation;
