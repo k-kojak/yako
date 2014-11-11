b4c7885ed259e7b279b0cf110622aa140b3318ab
Tamas Kojedzinszky <tamas.kojedzinszky@gmail.com>
2014-11-11 13:29:10 +0100

    ThreadView quick answer options are working.

--- src/hu/rgai/yako/handlers/ThreadContentGetterHandler.java
+++ src/hu/rgai/yako/handlers/ThreadContentGetterHandler.java
@@ -13,12 +13,12 @@ public class ThreadContentGetterHandler extends TimeoutHandler {
     this.mThreadDisplayer = mThreadDisplayer;
   }
   
-  public void onComplete(boolean saveToDbAfterLoad, boolean success, FullThreadMessage messageContent, boolean scrollToBottom) {
+  public void onComplete(boolean loadQuickAnswers, boolean saveToDbAfterLoad, boolean success, FullThreadMessage messageContent, boolean scrollToBottom) {
     if (!success) {
       Toast.makeText(mThreadDisplayer, "Error while loading content", Toast.LENGTH_LONG).show();
     } else {
       mThreadDisplayer.appendLoadedMessages(messageContent, saveToDbAfterLoad);
-      mThreadDisplayer.displayMessage(scrollToBottom, saveToDbAfterLoad);
+      mThreadDisplayer.displayMessage(loadQuickAnswers, scrollToBottom, saveToDbAfterLoad);
     }
     mThreadDisplayer.dismissProgressDialog();
   }
